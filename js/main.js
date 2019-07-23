38f427b7c9474768fbdb11f04b6e7afe598ad9fc
Danielle Moss <dani.j.moss@gmail.com>
2019-07-23 07:43:39 -0400

    added in animation settings and fixed blur error

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   let controller = new ScrollMagic.Controller();
 
   let timeline = new TimelineMax();
-  timeline.to("#sixth", 6, { y: -700 });
+  // position where the images will scroll to during animation
+  timeline
+    .to("#sixth", 6, { y: -700 })
+    .to("#fifth", 6, { y: -500 }, "-=6")
+    .to("#fourth", 6, { y: -400 }, "-=6")
+    .to("#third", 6, { y: -300 }, "-=6")
+    .to("#second", 6, { y: -200 }, "-=6")
+    .to("#first", 6, { y: -100 }, "-=6")
+    .to(".content, .blur", 6, { top: "0%" }, "-=6")
+    .to(".title, nav, .footer-wrapper", 6, { y: -600 }, "-=6");
 
   let scene = new ScrollMagic.Scene({
     triggerElement: ".section", // starts at section element
